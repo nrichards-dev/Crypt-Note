9b1dc7135f90955d79ea304ac709c51a4982f2fe
Noah <husknoah@gmail.com>
2025-02-24 23:04:53 -0330

    inital commit

--- main.py
+++ main.py
@@ -20,23 +20,24 @@ def main():
     root.configure(background='#262728')
     
     textbox = tk.Entry()
-    textbox.pack(anchor='nw')
+    textbox.insert(index=0, string='Hi')
+    #textbox.pack(anchor='nw')
 
     button = tk.Button(root, text="Create")
-    button.config(command=click, bg='#484E70', activebackground='#393A40')
-    button.pack(anchor='nw')
+    button.config(command=click, bg='#484E70', activebackground='#393A40', width=10)
+    button.grid(row=0, column=0, pady=(10, 5))
 
     button = tk.Button(root, text="Open")
-    button.config(command=click, bg='#484E70', activebackground='#393A40')
-    button.pack(anchor='nw')
+    button.config(command=click, bg='#484E70', activebackground='#393A40', width=10)
+    button.grid(row=1, column=0, pady=5)
 
     button = tk.Button(root, text="Save")
-    button.config(command=click, bg='#484E70', activebackground='#393A40')
-    button.pack(anchor='nw')
+    button.config(command=click, bg='#484E70', activebackground='#393A40', width=10)
+    button.grid(row=2, column=0, pady=5)
 
     button = tk.Button(root, text="Remove")
-    button.config(command=click, bg='#484E70', activebackground='#393A40')
-    button.pack(anchor='nw')
+    button.config(command=click, bg='#484E70', activebackground='#393A40', width=10)
+    button.grid(row=3, column=0, pady=5, padx=10)
 
     root.mainloop()
 
