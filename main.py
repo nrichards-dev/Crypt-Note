70c4ead5cfd4968b49e00b68f2e416e3d1820caa
Noah <husknoah@gmail.com>
2025-02-25 15:19:38 -0330

    ui changes

--- main.py
+++ main.py
@@ -25,20 +25,32 @@ def main():
 
     button = tk.Button(root, text="Create")
     button.config(command=click, bg='#484E70', activebackground='#393A40', width=10)
-    button.grid(row=0, column=0, pady=(10, 5))
+    button.pack(ipadx=0, ipady=0, padx=20, pady=10, anchor='w')
 
     button = tk.Button(root, text="Open")
     button.config(command=click, bg='#484E70', activebackground='#393A40', width=10)
-    button.grid(row=1, column=0, pady=5)
+    button.pack(ipadx=0, ipady=0, padx=20, pady=10, anchor='w')
 
     button = tk.Button(root, text="Save")
     button.config(command=click, bg='#484E70', activebackground='#393A40', width=10)
-    button.grid(row=2, column=0, pady=5)
+    button.pack(ipadx=0, ipady=0, padx=20, pady=10, anchor='w')
 
     button = tk.Button(root, text="Remove")
     button.config(command=click, bg='#484E70', activebackground='#393A40', width=10)
-    button.grid(row=3, column=0, pady=5, padx=10)
+    button.pack(ipadx=0, ipady=0, padx=20, pady=10, anchor='w')
 
+    scroll_bar = tk.Scrollbar(root)
+    mylist = tk.Listbox(root, yscrollcommand = scroll_bar.set ) 
+    for line in range(1, 26): 
+        mylist.insert('end', "Geeks " + str(line)) 
+    scroll_bar.config( command = mylist.yview ) 
+    mylist.pack(ipadx=1, ipady=0, pady=5, anchor='w', side='right')
+    
+    scroll_bar = tk.Scrollbar(root)
+    text_info = tk.Text(root, yscrollcommand=scroll_bar.set, height=10) 
+    text_info.pack(ipadx=0, ipady=2,side='right', anchor='n')
+    # configuring the scrollbar 
+    scroll_bar.config(command=text_info.yview, width=50) 
     root.mainloop()
 
 main()
